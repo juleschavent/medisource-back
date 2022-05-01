beb2bbfdaac106883dba2602507b330a535e3084
jules <juleschavent@gmail.com>
2022-05-01 19:49:34 +0200

    added env var for db

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
-    user: 'bofv3727_admin',
-    host: 'localhost',
-    password: 'a8K1#I3%66pR',
-    database: 'bofv3727_medisource-db',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 if (typeof(PhusionPassenger) !== 'undefined') {
