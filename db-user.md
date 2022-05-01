f1f2a9e4fff6b92ad4141af9af48e6e6aded8cce
jules <juleschavent@gmail.com>
2022-05-01 12:23:40 +0200

    first commit

---
+++ db-user.md
@@ -0,0 +1,3 @@
+phpmyadmin user =>
+id : admin
+pw : a8K1#I3%66pR
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+    user: 'bofv3727_admin',
+    host: 'chvt.me',
+    password: 'a8K1#I3%66pR',
+    database: 'bofv3727_medisource-db',
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001')
+})
+////////////////////////////////////////            READ
+// Test Query
+app.get('/systeme', (req, res) => {
+    db.query(`SELECT * FROM systeme`,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        })
+})
